24c5ea7d2c3cfa563db46032ecd9e58e957e7a62
Ivan Angelov <inferno16@mail.bg>
2018-12-14 18:42:15 +0200

    Added more things to the premake process and re-generated the project - Created batch script that calls premake to (re)generate the project - Added rule in the gitignore file for dlls in the project folders

---
+++ Generate Project.bat
@@ -0,0 +1,23 @@
+@ECHO OFF
+
+SET defaultAction=vs2017
+
+ECHO.
+ECHO Available actions: vs2005, vs2008, vs2010, vs2012, vs2013, vs2015, vs2017, gmake, xcode4, codelite
+ECHO Default action: %defaultAction%
+ECHO.
+
+SET /p option=Enter action and press enter (leave blank to use default):
+ECHO.
+
+IF [%option%] EQU [] (
+    SET action=%defaultAction%
+) ELSE (
+    SET action=%option%
+)
+SET option=
+
+CALL vendor\premake\premake5.exe %action%
+ECHO.
+
+PAUSE
--- premake5.lua
+++ premake5.lua
@@ -1,28 +1,11 @@
-workspace "MoteEmu"
-    configurations
-    {
-        "Debug32",
-        "Debug64",
-        "Release32",
-        "Release64"
-    }
-
-    filter "configurations:*32"
-        architecture "x86"
-
-    filter "configurations:*64"
-        architecture "x64"
-        
-    filter "system:windows"
-        cppdialect "C++17"
-        systemversion "latest"
-
-outputdir = "%{cfg.buildcfg}-%{cfg.system}"
+-- Global variables
+outputdir = "%{cfg.buildcfg}_%{cfg.system}_%{cfg.architecture}"
 inputdir = "%{prj.name}/src"
 
-project "MoteEmu"
-    location "MoteEmu"
-    kind "WindowedApp"
+-- Workspace definition (applies to all projects)
+workspace "MoteEmu"
+    configurations { "Debug", "Release" }
+    platforms { "x86", "x64" }
     language "C++"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
@@ -40,14 +23,37 @@ project "MoteEmu"
         ["Source Files"] = { "**.cpp", "**.cc", "**.c" }
     }
 
+    filter "platforms:x86"
+        architecture "x86"
+
+    filter "platforms:x64"
+        architecture "x64"
+        
+    filter "configurations:Debug"
+        kind "ConsoleApp"
+        defines { "DEBUG" }
+        optimize "Debug"
+
+    filter "configurations:Release"
+        kind "WindowedApp"
+        defines { "NDEBUG" }
+        optimize "Full"
+
+    filter "system:windows"
+        cppdialect "C++17"
+        systemversion "latest"
+
+
+-- Project definitions
+project "MoteEmu"
+    location "MoteEmu"
+
     includedirs { "vendor/SDL2/include" }
     links { "SDL2", "SDL2main" }
 
-    filter "configurations:*32"
+    filter "platforms:x86"
         libdirs { "vendor/SDL2/lib/x86" }
 
-    filter "configurations:*64"
+    filter "platforms:x64"
         libdirs { "vendor/SDL2/lib/x64" }
-    
-
         
